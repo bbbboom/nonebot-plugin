db3a811b729e95a4f4528f99ec075d5724351d27
fz6m <fz6m@qq.com>
2020-05-13 18:24:41 +0800

    update cp story

---
+++ CQCp/cp/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import ujson
+import os
+import random
+
+async def getParameters(msg):
+    try:
+        list = msg.split(' ')
+        if list[0] == 'cp' and len(list) == 3:
+            return [True, list[1], list[2]]
+    except:
+        pass
+    return [False]
+
+async def readInfo(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            return ujson.loads(f.read().strip())
+    raise Exception
+
+async def getMessage(bot, userGroup, msg):
+    parameter = await getParameters(msg)
+    if parameter[0] == False:
+        return
+    content = await readInfo('./cp/data/content.json')
+    content = random.choice(content['data']).replace('<攻>', parameter[1]).replace('<受>', parameter[2])
+    await bot.send_group_msg(group_id = int(userGroup), message = str(content))
+
+    
