944d51a1d9bd1b4df7176d3e468b9e4d89cd9471
fz6m <fz6meng@gmail.com>
2020-06-22 02:31:54 +0800

    update vtb fortune

---
+++ CQVortune/vortune/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from nonebot import *
+from . import main
+from . import utils
+
+bot = get_bot()
+
+@bot.on_message("group")
+async def entranceFunction(context):
+    msg = str(context["message"])
+    userGroup = context["group_id"]
+    userQQ = context["user_id"]
+    await utils.initialization()
+    await main.handlingMessages(msg, bot, userGroup, userQQ)
