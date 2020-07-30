56c723760c8192c6503a578deb57187d530c94cb
fz6m <fz6meng@gmail.com>
2020-07-30 23:16:08 +0800

    update throw&creep plugin

--- CQThrow&Creep/bot.py
+++
@@ -1,7 +0,0 @@
-import nonebot
-import config
-
-if __name__ == "__main__":
-    nonebot.init(config)
-    nonebot.load_plugins("mybot/plugins", 'mybot.plugins')
-    nonebot.run(host="0.0.0.0", port=8080)
